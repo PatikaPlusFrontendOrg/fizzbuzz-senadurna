8c89a812566d4c4f45f3de35025eabc99f700889
senadurna <senanurdurnaa@gmail.com>
2025-07-31 00:38:46 +0300

    FizzBuzz if ve switch çözümleri eklendi

--- src/fizzbuzzSwitch.js
+++ src/fizzbuzzSwitch.js
@@ -1,5 +1,21 @@
 function fizzbuzzSwitch(num) {
-    // Kodlar buraya gelecek
+  let result = "";
+
+  switch (true) {
+    case num % 3 === 0 && num % 5 === 0:
+      result = "FizzBuzz";
+      break;
+    case num % 3 === 0:
+      result = "Fizz";
+      break;
+    case num % 5 === 0:
+      result = "Buzz";
+      break;
+    default:
+      result = num;
+  }
+
+  return result;
 }
 
 module.exports = fizzbuzzSwitch;
